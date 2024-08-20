6ba31a7758b6afc3b9112aaa626e5cb2af82a07d
moeed-khawaja <153828012+moeed-khawaja@users.noreply.github.com>
2024-08-20 11:15:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,16 @@ import os
 
 app = Flask(__name__)
 
-# Environment variable with correct name (case-sensitive)
 redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_password = os.getenv('REDIS_PASSWORD', None)
 
-# Connect to Redis using environment variables or defaults
-try:
-    redis = Redis(host=redis_host, port=6379, password=redis_password)
-except ConnectionError as e:
-    print(f"Error connecting to Redis: {e}")
-    redis = None  # Handle the error appropriately
+redis = Redis(host=redis_host, port=6379, password=redis_password)
 
 @app.route('/')
 def hello():
-    """Increments a counter in Redis and displays the hit count."""
+    count = redis.incr('hits')
+    return f'Hello! This page has been visited {count} times.'
 
-    if redis:  # Check if Redis connection is successful
-        count = redis.incr('hits')
-        return f'Hello! This page has been visited {count} times.'
-    else:
-        return 'Error: Could not connect to Redis cache.'
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.getenv('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
